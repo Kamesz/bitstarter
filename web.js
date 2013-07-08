ead349e75817f3399ba377d0e3f7f5fa9eef2372
Kamesz <kamesz@gmail.com>
2013-07-08 00:27:34 +0000

    web.js change

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
+
+var text = fs.readFileSync("index.html");
+var buffer = new Buffer( text, 'utf-8');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+    response.send(buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
