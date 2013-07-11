7f40daae7458a86219026ccd429bdb1ca1d6179a
Kamesz <kamesz@gmail.com>
2013-07-11 23:00:02 +0000

    grader.js is working

--- grader.js
+++ grader.js
@@ -2,8 +2,28 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = 'checks.json';
+var URL_DEFAULT = 'http://protected-beach-2673.herokuapp.com/';
+
+
+var checkUrl = function( url )
+{
+	if( url.length == 0 )
+	{
+		console.log("Url is empty." );
+	}
+}
+
+var download = function(url)
+{
+	checkUrl(url);
+	restler.get(url).on('complete', function(myherokuhtml) {
+		return myherokuhtml;
+	});
+}
+
 
 var assertFileExists = function(infile) {
 	var instr = infile.toString();
@@ -16,7 +36,7 @@ var assertFileExists = function(infile) {
 }
 
 var cheerioHtmlFile = function(htmlfile) {
-	return cheerio.load(fs.readFileSync(htmlfile));
+	return cheerio.load(htmlfile);
 }
 
 var loadChecks =function(checksfile) {
@@ -45,11 +65,13 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --url <html_file>', 'Path to index.html' )
+
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+   var checkJson = checkHtmlFile(download(program.url), program.checks);
+   var outJson = JSON.stringify(checkJson, null, 4);
+   fs.writeFileSync("output.txt", outJson);
+   console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
